5729de6b957b164e12226395d2f2c3cfcae39ce4
Jackson Lorran <computadordb@gmail.com>
2024-01-06 19:54:22 -0300

    Objeto

---
+++ js-básico/aula8 - objetos/index.js
@@ -0,0 +1,23 @@
+const carro = {
+  marca:"ford",
+  modelo:"esportivo",
+  ano: 2015,
+  placa :"ABC-1234",
+
+  buzina: function(){
+    alert('beeeeee')
+  },
+
+  completo: function(){
+    alert("Esse carro é um " + this.marca + " modelo: "+ this.modelo + " ano: " + this.ano)
+  }
+};
+
+console.log(carro)
+console.log(carro['marca']) //acessando elemento do objeto carro
+console.log(carro.completo())
+// console.log(carro.buzina())
+
+class jogo{
+
+}
