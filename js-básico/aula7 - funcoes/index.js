4fd4254c3feb84a74608076a2cfb5028a461cf44
Jackson Lorran <computadordb@gmail.com>
2024-01-06 19:28:26 -0300

    funcoes

---
+++ js-básico/aula7 - funcoes/index.js
@@ -0,0 +1,15 @@
+function soma(valor1, valor2){
+  return valor1 + valor2
+}
+document.getElementById('texto').innerHTML = soma(10,20)
+
+
+function cotacaoDolar(real, cotacaoDolar) {
+  return real*cotacaoDolar
+}
+
+// alert(cotacaoDolar(20,5)) 100
+
+function alerta(){
+  alert('hello')
+}
