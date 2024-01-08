29a53c4ac072ee7b1d1e823de54ed0fb00f6dc1c
Jackson Lorran <computadordb@gmail.com>
2024-01-08 09:50:12 -0300

    aula 10 exercicio

---
+++ js-básico/aula10 - exercicio/index.js
@@ -0,0 +1,24 @@
+var precoCombustível = 5
+var gastoComubstível = 10
+var distancia = 100
+var tipoCombustível = 'etanol'
+console.log((distancia/gastoComubstível)*precoCombustível)
+
+
+
+if(tipoCombustível==='etanol'){
+  console.log((distancia/gastoComubstível)*4)
+}
+
+var peso = 100
+var altura = 2
+
+console.log(peso/(altura**2))
+
+
+
+let nomeDoAluno = 'Renan Johannsen de Paula';
+console.log(nomeDoAluno);
+
+nomeDoAluno = 'Vitor Johansen Guerra';
+console.log(nomeDoAluno);
