6570da0f2622fd8e14a27dafa3b7c7d079365015
Jackson Lorran <computadordb@gmail.com>
2024-01-08 15:02:14 -0300

    js intermediario

---
+++ js-intermediario/aula14-arrays/index.js
@@ -0,0 +1,16 @@
+
+// Lista de strings
+
+//position=0       1         2
+var array  = ['Joao', 'lucas','bernado',]
+
+// atibuindo a posicao 5 da lista um valor
+lista[5] ='lucio'
+
+// Adicionando elemento ao final de uma lista
+lista.push('renan')
+lista.push(23)
+
+// removendo o último item da lista 
+lista.pop()
+console.log(lista)
