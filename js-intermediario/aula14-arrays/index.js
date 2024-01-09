54696601b483d47f488d94e28d408008bb0932ae
Jackson Lorran <computadordb@gmail.com>
2024-01-09 08:43:37 -0300

    arrays

--- js-intermediario/aula14-arrays/index.js
+++ js-intermediario/aula14-arrays/index.js
@@ -1,16 +1,33 @@
 
-// Lista de strings
+// // Lista de strings
 
-//position=0       1         2
-var array  = ['Joao', 'lucas','bernado',]
+// //position=0       1         2
+// var array  = ['Joao', 'lucas','bernado',]
 
-// atibuindo a posicao 5 da lista um valor
-lista[5] ='lucio'
+// // atibuindo a posicao 5 da lista um valor
+// lista[5] ='lucio'
 
-// Adicionando elemento ao final de uma lista
-lista.push('renan')
-lista.push(23)
+// // Adicionando elemento ao final de uma lista
+// lista.push('renan')
+// lista.push(23)
 
-// removendo o último item da lista 
-lista.pop()
-console.log(lista)
+// // removendo o último item da lista 
+// lista.pop()
+// console.log(lista)
+
+// // =========================
+// // percorrendo uma palavraa com for
+
+// const palavra ='extraterrestre'
+// for (let index = 0; index < palavra.length; index++) {
+//   console.log(palavra[index]);
+  
+// }
+
+for (let j = 1; j < 10; j++) {
+  for (let i = 1; i < 10; i++) {
+    if(i%2===0 && j%2===0 )
+    console.log(`A multiplicação entre ${j} x ${i} resulta em: `+ j*i)
+  }
+  
+}
---
+++ js-intermediario/aula15/index.js
@@ -0,0 +1,13 @@
+const notas = [];
+
+notas.push(5)
+notas.push(6)
+notas.push(4)
+notas.push(3)
+notas.push(5)
+notas.push(2)
+notaTotal=0
+for(i=0; i<notas.length; i++){
+  notaTotal += notas[i]
+}
+console.log(notaTotal/notas.length)
