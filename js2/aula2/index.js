e1d7339d6849f56c80e64f2956bc255e253d7e47
Jackson Lorran <computadordb@gmail.com>
2024-01-11 09:19:04 -0300

    comecando uma nova jornada'

---
+++ js2/aula2/index.js
@@ -0,0 +1,7 @@
+// Formas de mostrar o conteúdo na tela
+console.log('comecando');
+console.log("comecando");
+console.log("comecando 'agora'");
+console.log(`comecando`)
+console.log(1223455)
+console.log(1.532312)
