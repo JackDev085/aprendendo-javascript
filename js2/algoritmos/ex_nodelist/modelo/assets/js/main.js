9b92dbc5114ce8530e875cba7dd98652519c9a53
Jackson Lorran <computadordb@gmail.com>
2024-01-16 14:05:56 -0300

    ex nodelist

---
+++ js2/algoritmos/ex_nodelist/modelo/assets/js/main.js
@@ -0,0 +1,16 @@
+const paragrafos = document.querySelector('.paragrafos')
+const p = paragrafos.querySelectorAll('p')
+
+// Extraindo todos os estilos aplicados ao body
+const estiloBody = getComputedStyle(document.body);
+// Separando um estilo em especifico do body
+const backgroundColorBody =estiloBody.background
+
+// Atribuindo a cada elemento p da div pagrafos e aplicando um estilo(backgroundColorBody)
+for(i in p){
+  p[i].style.background=backgroundColorBody
+  p[i].style.color="white"
+
+
+}
+
