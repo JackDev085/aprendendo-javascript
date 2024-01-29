78f39ed436a56fe5b37fe1098e8f179618c08d26
Jackson Lorran <computadordb@gmail.com>
2024-01-29 20:32:21 -0300

    novo topico: objetos avancado

---
+++ objetos_ avancado/revisando_objetos.js
@@ -0,0 +1,26 @@
+const pessoa = {
+  nome:"Luiz",
+  sobrenome: "Júlio"
+};
+// console.log(pessoa.nome);
+console.log(pessoa['nome'])
+console.log(pessoa.sobrenome)
+// console.log(pessoa['sobrenome'])
+// outra forma de criar um objeto
+const pessoa1 = new Object()
+pessoa1.nome="Julio"
+pessoa1.sobrenome="alex"
+console.log(pessoa1, pessoa)
+
+// factory function
+function criaPessoa(nome,sobrenome){
+  return{
+  nome,
+  sobrenome,
+  get nomeCompleto(){
+   return`${this.nome} ${this.sobrenome}`
+  }
+}
+}
+p1 = criaPessoa("Julio", "Alberto")
+console.log(p1.nomeCompleto)
